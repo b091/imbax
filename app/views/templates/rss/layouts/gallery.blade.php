683ed9b2b440b0a1f4ccbb7c7b52ef649fcebbca
b091 <skrzypkowiak@gmail.com>
2013-10-25 02:59:32 +0200

    modyfikacje galeri

--- app/views/templates/rss/layouts/gallery.blade.php
+++ app/views/templates/rss/layouts/gallery.blade.php
@@ -33,23 +33,132 @@
         </div>
     </div>
 </div>
+<style type="text/css">
+    #showcase {
+        margin-top: 70px;
+    }
 
+    #showcase .container {
+        /*width: 980px;*/
+    }
+
+    #showcase .image img {
+        box-shadow: 3px 3px 5px 0px #B7B6B7;
+    }
+
+    #showcase .info {
+        margin-top: 30px;
+    }
+
+    #showcase .info h4 {
+        font-size: 17px;
+    }
+
+    #showcase .info p {
+        color: #333333;
+        line-height: 21px;
+        font-size: 13px;
+    }
+
+    #showcase .features_op1_row .last {
+        margin-right: 0;
+    }
+
+    #showcase .features_op1_row .feature .text {
+        margin: 32px auto 0;
+        width: 87%;
+    }
+
+    #showcase .features_op1_row .feature .text h6 {
+        margin: 0 0 14px 0;
+        font-size: 20px;
+    }
+
+    #showcase .features_op1_row .feature .text p {
+        margin: 0;
+        font-size: 14px;
+        line-height: 23px;
+    }
+
+    #showcase .features_op1_row .feature .img_box {
+        position: relative;
+        border-radius: 5px;
+    }
+
+    #showcase .features_op1_row .feature .img_box img {
+        border-radius: 5px;
+        max-width: 98%;
+        margin: 0 auto;
+        display: block;
+    }
+
+    #showcase .features_op1_row .feature .img_box span.circle {
+        background: rgba(254, 186, 31, 0.6);
+        width: 48px;
+        height: 48px;
+        display: none;
+        text-align: center;
+        position: absolute;
+        top: 30%;
+        left: 38%;
+        border-radius: 100%;
+    }
+
+    #showcase .features_op1_row .feature .img_box span.circle span.plus {
+        display: inline-block;
+        font-weight: bold;
+        color: white;
+        font-size: 32px;
+        margin-top: -3px;
+    }
+
+    #showcase .features_op1_row .feature .img_box:hover span.circle {
+        display: block;
+    }
+
+    #showcase .features_op1_row .feature .img_box:hover .img_box {
+        box-shadow: 0px 0px 1px 100px black inset;
+    }
+
+    #showcase .features_op1_row .feature .img_box:hover img {
+        box-shadow: 1px 1px 6px 0px #888;
+        border-radius: 5px;
+        opacity: 0.9;
+        filter: alpha(opacity=90);
+    }
+</style>
 
 @section('content')
 <br/>
-<div id="links" class="row">
-    @foreach($gallery as $key => $image)
 
-    <div class="col-sm-6 col-md-3" style="margin-top: 10px;">
-        <div class="thumbnail">
-            <a href="/files/gallery/{{$image->photo}}" title="{{$image->name}}" data-gallery>
-                <img src="/files/gallery/{{$image->photo}}" alt="{{$image->name}}" width="253px" class="img-responsive"/>
-                <div class="caption">
-                    {{$image->name}}
+<div id="showcase">
+    <div class="container">
+
+        <div class="row feature_wrapper">
+            <!-- Features Row -->
+            <div class="features_op1_row">
+                @foreach($gallery as $key => $image)
+
+                <div class="col-sm-6 col-md-3 feature" style="margin-top: 10px;">
+                    <div class="thumbnail img_box">
+                        <a href="/files/gallery/{{$image->photo}}" title="{{$image->name}}" data-gallery>
+                            <img src="/files/gallery/{{$image->photo}}" alt="{{$image->name}}" width="253px" class="img-responsive"/>
+                            <span class="circle">
+                                <span class="plus">&#43;</span>
+                            </span>
+
+                            <div class="caption">
+                                {{$image->name}}
+                            </div>
+                        </a>
+                    </div>
                 </div>
-            </a>
+                @endforeach
+
+            </div>
         </div>
     </div>
-    @endforeach
+    <br/><br/>
 </div>
+
 @stop

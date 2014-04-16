0d904d114d2a3f7c3a02550217f38a8d131ad753
b091 <skrzypkowiak@gmail.com>
2014-04-16 19:31:28 +0200

    cleaning :]

--- app/views/admin/layout.blade.php
+++ app/views/admin/layout.blade.php
@@ -4,19 +4,20 @@
     <meta charset="utf-8">
     <title>Laravel4 AngularJS</title>
     <link href="/imbaxadm/css/app.css" rel="stylesheet" />
-    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular.js"></script>
-    <script src="http://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.10.0.js"></script>
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
 
-    <script src="/imbaxadm/lib/angular/angular-route.js"></script>
-    <script src="/imbaxadm/lib/angular/angular-resource.js"></script>
-    <script src="/imbaxadm/lib/angular/angular-sanitize.js"></script>
+    <div class="container" ng-view></div>
 
+    <script src="//code.angularjs.org/1.2.16/angular.js"></script>
+    <script src="//code.angularjs.org/1.2.16/angular-route.js"></script>
+    <script src="//code.angularjs.org/1.2.16/angular-resource.js"></script>
+    <script src="//code.angularjs.org/1.2.16/angular-sanitize.js"></script>
+    <script src="//angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.10.0.js"></script>
     <script src="/imbaxadm/js/app.js"></script>
     <script src="/imbaxadm/js/controller.js"></script>
     <script src="/imbaxadm/js/services.js"></script>
-</head>
-<body>
-    <div class="container" ng-view></div>
+
 </body>
 </html>

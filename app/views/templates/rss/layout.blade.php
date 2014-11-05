b7501617074397bda7a77153f51b30c9460dc97c
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2014-11-05 16:27:37 +0100

    bower+ gulp added - path fixes

--- app/views/templates/rss/layout.blade.php
+++ app/views/templates/rss/layout.blade.php
@@ -7,20 +7,21 @@
     <meta name="description" content="{{@$pageDescription}}">
     <meta name="Keywords" content="{{@$pageKeywords}}">
     <link rel="shortcut icon" href="/{{$templateDir}}/images/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="/assets/famfamfam_flags/famfamfam-flags.css">
+    <link rel="stylesheet" type="text/css" href="/assets/bower_vendor/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/assets/bower_vendor/famfamfam-flags-sprite/src/flags-sprite.css">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,700,300">
     <link rel="stylesheet" type="text/css" href="/{{$templateDir}}/css/rss.css"/>
-    <link rel="stylesheet" type="text/css" href="/packages/blueimp/Gallery/css/blueimp-gallery.min.css">
-    <link rel="stylesheet" type="text/css" href="/packages/gallery/css/bootstrap-image-gallery.min.css">
+    <link rel="stylesheet" type="text/css" href="/assets/bower_vendor/blueimp-bootstrap-image-gallery/css/bootstrap-image-gallery.css">
+    <link rel="stylesheet" type="text/css" href="/assets/bower_vendor/blueimp-gallery/css/blueimp-gallery.css">
 </head>
 
 <body>
+
 <div class="row topbar-rss">
     <div class="container">
         <div class="col-xs-6 col-sm-3 topbar-text-rss">
             @foreach($langs as $item)
-            <a href="/{{$item->code}}/"><i class="famfamfam-flag-{{$item->code}}"></i></a>
+            <a href="/{{$item->code}}/"><span class="flag-icon {{$item->code}}"></span></a>
             @endforeach
         </div>
         <div class="col-md-9 col-xs-6 col-sm-9 hidden-sm hidden-xs">
@@ -38,6 +39,8 @@
 @yield('topmenu')
 @include('templates.rss.topbanner')
 
+<script src="/assets/bower_vendor/jquery/dist/jquery.js"></script>
+
 <div class="col-md-12" style="background-color: #ffffff;">
     <div class="container">
         @yield('content')
@@ -47,11 +50,11 @@
 
 @include('templates.rss.footer')
 
-<script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="/assets/bower_vendor/bootstrap/dist/js/bootstrap.js"></script>
 <script src="/templates/rss/js/app.js"></script>
-<script src="/packages/gallery/js/jquery.blueimp-gallery.min.js"></script>
-<script src="/packages/gallery/js/bootstrap-image-gallery.min.js"></script>
+<script src="/assets/bower_vendor/blueimp-gallery/js/blueimp-gallery.js"></script>
+<script src="/assets/bower_vendor/blueimp-gallery/js/jquery.blueimp-gallery.js"></script>
+<script src="/assets/bower_vendor/blueimp-bootstrap-image-gallery/js/bootstrap-image-gallery.js"></script>
 
 </body>
 </html>
